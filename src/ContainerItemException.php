70680ab5d2ef5e59b8f9777b3b66b77a8a81e0b2
Felipe Martins <me@fefas.net>
2016-12-27 20:23:02 -0200

    Add type hint on Exception contructor

--- src/ContainerItemException.php
+++ src/ContainerItemException.php
@@ -6,7 +6,7 @@ use Interop\Container\Exception\ContainerException;
 
 abstract class ContainerItemException extends \Exception implements ContainerException
 {
-    public function __construct($label)
+    public function __construct(string $label)
     {
         parent::__construct(sprintf($this->messageTemplate(), $label));
     }
